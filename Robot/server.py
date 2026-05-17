3b98b022603244681f8470345bc58b089a3137a3
enzon <pasteque9000@gmail.com>
2026-05-17 13:14:26 +0200

    server and first interface live

--- Robot/server.py
+++ Robot/server.py
@@ -1,29 +1,33 @@
-from flask import Flask
-from flask_socketio import SocketIO
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from sensors.gyro import get_data
+import time
+import threading
 app = Flask(__name__)
+
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
-    return """
-    <h1>Robot Server</h1>
-    <script src="https://cdn.socket.io/4.6.0/socket.io.min.js"></script>
-    <script>
-        // JS côté navigateur — se connecte au serveur SocketIO
-        const socket = io();
-        
-        socket.on('connect', () => {
-            console.log('Connected to Pi!');
-            document.body.innerHTML += '<p>Connected!</p>';
-        });
-    </script>
-    """
+    return render_template("index.html")  # charge le fichier HTML
 
 @socketio.on("connect")
 def on_connect():
-    # Cette fonction s'exécute sur le Pi quand le navigateur se connecte
     print("[INFO] PC connected!")
+    # Lance le stream dans un thread séparé
+    thread = threading.Thread(target=gyro_event, daemon=True)
+    thread.start() 
+
+
+def gyro_event():
+    while True:
+        data = get_data()
+        socketio.emit("gyro", data)
+        time.sleep(0.05)  
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
+
+#bajbdshpbfqhfshf
+## TO DO : reconnecté le rasp en server , vérifié que le code est bien deployé , executé server sur le pi (derniere modif : on a supp les print de gyro pour voir si ça libère le terminal 
+# et on aussi supp le consol log de socket on gyro dans server en javascript ) , checker les consoles et la page html si elle marche et demandé à claude pk la connection et la page de s'affiche plus (suspection boucle infinie)   
--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,10 @@
 "C:\Program Files (x86)\WinSCP\WinSCP.com" /command ^
     "open sftp://enzon:gumball@192.168.1.172/" ^
     "put C:\raspb\Repo\Robot\*.py /home/enzon/" ^
+    "put C:\raspb\Repo\Robot\sensors\*.py /home/enzon/sensors/" ^
+    "put C:\raspb\Repo\Robot\motors\*.py /home/enzon/motors/" ^
+    "put C:\raspb\Repo\Interface\templates\index.html /home/enzon/templates/" ^
+    "put C:\raspb\Repo\Interface\static\style.css /home/enzon/static/" ^
     "exit"
 echo Transfert terminé !
 pause
