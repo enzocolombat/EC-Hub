f3d104de7c76d1bcefbd84c4631b8d1481d270cc
enzon <pasteque9000@gmail.com>
2026-05-18 15:10:17 +0200

    Remove sensitive data - use .env file

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,11 @@
 @echo off
+for /f "tokens=1,2 delims==" %%a in (.env) do set %%a=%%b
 "C:\Program Files (x86)\WinSCP\WinSCP.com" /command ^
-    "open sftp://enzon:gumball@192.168.1.172/" ^
-    "put C:\raspb\Repo\Robot\*.py /home/enzon/" ^
+    "open sftp://%PI_USER%:%PI_PASS%@%PI_IP%/" ^
+    "put C:\raspb\Repo\Robot\server.py /home/enzon/" ^
     "put C:\raspb\Repo\Robot\sensors\*.py /home/enzon/sensors/" ^
-    "put C:\raspb\Repo\Robot\motors\*.py /home/enzon/motors/" ^
-    "put C:\raspb\Repo\Interface\templates\index.html /home/enzon/templates/" ^
-    "put C:\raspb\Repo\Interface\static\style.css /home/enzon/static/" ^
+    "put C:\raspb\Repo\Interface\templates\*.html /home/enzon/templates/" ^
+    "put C:\raspb\Repo\Interface\static\*.css /home/enzon/static/" ^
     "exit"
 echo Transfert terminé !
 pause
