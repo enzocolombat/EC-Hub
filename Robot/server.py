00f7ce4fca27b5383618f74725d232f1e7fd18e6
enzon <pasteque9000@gmail.com>
2026-05-18 15:29:04 +0200

    translat #

--- Robot/server.py
+++ Robot/server.py
@@ -9,12 +9,13 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
-    return render_template("index.html")  # charge le fichier HTML
+    return render_template("index.html") #Load HTML file
 
 @socketio.on("connect")
 def on_connect():
     print("[INFO] PC connected!")
-    # Lance le stream dans un thread séparé
+    # Send the stream in another thread
+
     thread = threading.Thread(target=gyro_event, daemon=True)
     thread.start() 
 
@@ -34,6 +35,3 @@ def gyro_event():
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
 
-#bajbdshpbfqhfshf
-## TO DO : reconnecté le rasp en server , vérifié que le code est bien deployé , executé server sur le pi (derniere modif : on a supp les print de gyro pour voir si ça libère le terminal 
-# et on aussi supp le consol log de socket on gyro dans server en javascript ) , checker les consoles et la page html si elle marche et demandé à claude pk la connection et la page de s'affiche plus (suspection boucle infinie)   
